876c234334bdbbf0bfccfe994d9deb34670bf3fc
Alex Merecka <merecka@gmail.com>
2019-10-04 19:55:36 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  let some_text = document.querySelector("p#text");
+  some_text.innerHTML = "This is really cool!";
+});
